e8cb311604a559c60a94f1bf8f4f5a6f856326dc
Fauzan Lubis <fauzanfauh45@gmail.com>
2017-02-08 21:30:06 +0700

    Ups, wrong number

--- Magic Grid Calculation/magic.php
+++ Magic Grid Calculation/magic.php
@@ -4,7 +4,7 @@ Grid persegi
 by Fauh45
 */
 $jumlah = 0; //Menyimpan Jumlah yang dihasilkan oleh loop
-$n = 6; //Untuk blok 4x4
+$n = 4; //Untuk blok 4x4
 for($i=0;$i<=$n;$i++) {
 	$jumlah += $i*$i;
 }
